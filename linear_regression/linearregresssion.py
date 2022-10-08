d1d603de05f899c56233709a3207c3552944cae2
StoneBam <fynn_wulf@gmx.de>
2022-10-08 17:36:18 +0200

    Add docstrings

--- linear_regression/linearregresssion.py
+++ linear_regression/linearregresssion.py
@@ -8,6 +8,22 @@ import numpy as np
 class LinearRegression():
 
     def __init__(self, data: list[float | int], initial_slope: float | None = None, initial_stepw: float | None = None) -> None:
+        """Tools to solve linear regression in uniform data.
+
+        Args:
+            data (list[float  |  int]):
+                list containing y values. Indicies will be x values.
+
+            initial_slope (float | None, optional):
+                If no initial slope for guess work is given,
+                the value will be automatically determined.
+                Defaults to None.
+
+            initial_stepw (float | None, optional):
+                If no initial step width for the algorithm is given,
+                the value will be automatically determined.
+                Defaults to None.
+        """
         self._data: list[float | int] = data
 
         self._init_slope: float = self.guess_initial_slope() if initial_slope is None else initial_slope
@@ -25,12 +41,36 @@ class LinearRegression():
         self._plot = plt.figure("Linear Regression")
 
     def guess_initial_slope(self, data: list[float | int] | None = None) -> float:
+        """Uses the maximum value and length of data combined as slope.
+
+        Args:
+            data (list[float  |  int] | None, optional):
+                List containing y values. Indicies will be x values.
+                If None, data from constuctor will be used.
+                Defaults to None.
+
+        Returns:
+            float: Slope of regression.
+        """
         max_y_value: int | float = max(self._data if data is None else data)
         max_x_value: int = len(self._data if data is None else data)
         slope: float = max_y_value / max_x_value
         return slope
 
     def guess_initial_stepw(self, initial_slope: float, fraction: float = 0.1) -> float:
+        """Uses a given fracion of the initial slope as step width.
+
+        Args:
+            initial_slope (float):
+                Initial slope.
+
+            fraction (float, optional):
+                Fracion of initial slope for initial step width.
+                Defaults to 0.1.
+
+        Returns:
+            float: step width
+        """
         if fraction > 1:
             return initial_slope
         elif fraction < 0:
@@ -39,6 +79,20 @@ class LinearRegression():
             return initial_slope * fraction
 
     def calc_current_error(self, current_slope: float, data: list[float | int] | None = None) -> float:
+        """Calculate current squared error of linear regression to data.
+
+        Args:
+            current_slope (float):
+                Current slope of linear regression.
+
+            data (list[float  |  int] | None, optional):
+                List containing y values. Indicies will be x values.
+                If None, data from constuctor will be used.
+                Defaults to None.
+
+        Returns:
+            float: Error squared of distance to line.
+        """
         error: float = 0.0
         y_val: int | float
         for x_val, y_val in enumerate(self._data if data is None else data):
@@ -46,19 +100,61 @@ class LinearRegression():
         return error
 
     def calc_next_stepw(self, old_stepw: float, error_diff: float) -> float:
+        """Calculate next step width.
+
+        Args:
+            old_stepw (float):
+                Old step width.
+
+            error_diff (float):
+                Error difference between old slope and new slope.
+
+        Returns:
+            float: step width.
+        """
         if error_diff < 0:
             return old_stepw / -2
         else:
             return old_stepw
 
     def comp_errors(self, last_error: float | None = None, curr_error: float | None = None) -> tuple[float, float]:
+        """Comparing errors of current and last iteration.
+
+        Args:
+            last_error (float | None, optional):
+                squared error of last iteration. Defaults to None.
+
+            curr_error (float | None, optional):
+                squared error of current iteration. Defaults to None.
+
+        Returns:
+            tuple[float, float]: difference, ratio
+        """
         _last_error: float = self._last_error if last_error is None else last_error
         _curr_error: float = self._curr_error if curr_error is None else curr_error
         diff: float = _last_error - _curr_error
         frac: float = _curr_error / _last_error
         return (diff, frac)
 
-    def recursive_approx(self, stepping: int | None = None, break_margin: float = 0.01) -> tuple[float, float, float]:
+    def recursive_approx(self, stepping: int | None = None, break_margin: float = 0.01) -> tuple[float, float, int]:
+        """Pseudo-recursivly iterate over data and modify regression.
+
+        Args:
+            stepping (int | None, optional):
+                Number representing after how many steps the method
+                should render the output to a plot.
+                Defaults to None.
+
+            break_margin (float, optional):
+                Number representing the margin from 1,
+                where the error ratio of the 2 last current/previous
+                error must lie in.
+                Defaults to 0.01.
+
+        Returns:
+            tuple[float, float, int]:
+                Current slope, current error, iterations
+        """
         _upper_margin: float = 1.0 + break_margin
         _lower_margin: float = 1.0 - break_margin
 
@@ -100,6 +196,11 @@ class LinearRegression():
             return self.recursive_approx(stepping)
 
     def render_diagram(self, slope: float) -> None:
+        """Plots of data and regression, as well as errors to stepwidth.
+
+        Args:
+            slope (float): current slope.
+        """
         self._plot.clf()
 
         x_value: np.ndarray = np.array(range(len(self._data)))
@@ -136,7 +237,6 @@ class LinearRegression():
         self._plot.add_subplot(sub_2)
 
         self._plot.show()
-        return None
 
 
 class TestLinearRegression:
@@ -183,7 +283,27 @@ def generate_random_data(
         function: Callable | None = None,
         noise: int = 0
         ) -> list:
+    """Random data generator for linear regression method
 
+    Args:
+        seed (str | None, optional):
+            random seed, to produce same data. Defaults to None.
+        n_datapoints (int, optional):
+            Amount of points for random cloud. Defaults to 100.
+        lower_bound (int, optional):
+            Lowest number of range. Defaults to 0.
+        upper_bound (int, optional):
+            Highest number of range. Defaults to 100.
+        function (Callable | None, optional):
+            function for data generator.
+            If None, data will have a random shape.
+            Defaults to None.
+        noise (int, optional):
+            Random standard deviation around datapoints. Defaults to 0.
+
+    Returns:
+        list: data with random y values.
+    """
     rng = random.Random(seed)
     if function is not None:
         stepsize = (upper_bound - lower_bound) // n_datapoints
