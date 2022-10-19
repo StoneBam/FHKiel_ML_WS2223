89687bf357a5a1d75361da72885c39e487e9b48f
Fynn <fynn_wulf@gmx.de>
2022-10-19 10:34:33 +0200

    Add while loop to be able to change test.png between iters

--- polygons_cnn/predict.py
+++ polygons_cnn/predict.py
@@ -11,28 +11,29 @@ CLASS_NAMES = ['hexagon', 'pentagon', 'square', 'triangle']
 
 
 if __name__ == '__main__':
-    img = tf.keras.utils.load_img(
-        os.path.join(DATA_DIR, "test.png"), target_size=(IMG_WIDHT, IMG_HEIGHT)
-    )
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)  # Create a batch
-
     model: tf.keras.Model = tf.keras.models.load_model(os.path.join(DATA_DIR, 'model.h5'))
-
     model.summary()
 
-    predictions = model.predict(img_array)
-    print(predictions)
-    score = tf.nn.softmax(predictions[0])
-    print(score)
+    while True:
+        img = tf.keras.utils.load_img(
+            os.path.join(DATA_DIR, "test.png"), target_size=(IMG_WIDHT, IMG_HEIGHT)
+        )
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)  # Create a batch
 
-    print(f"This image most likely belongs to {CLASS_NAMES[np.argmax(score)]} with a {100 * np.max(score):.2f} percent confidence.")
+        predictions = model.predict(img_array)
+        print(predictions)
+        score = tf.nn.softmax(predictions[0])
+        print(score)
 
-    plt.figure()
-    plt.subplot(1, 2, 1)
-    plt.title('Picture')
-    plt.imshow(img)
-    plt.subplot(1, 2, 2)
-    plt.title('Prediction')
-    plt.bar(CLASS_NAMES, score)
-    plt.show()
+        print(f"This image most likely belongs to {CLASS_NAMES[np.argmax(score)]} with a {100 * np.max(score):.2f} percent confidence.")
+
+        plt.figure(figsize=(10, 6))
+        plt.subplot(1, 2, 1)
+        plt.title('Picture')
+        plt.imshow(img)
+        plt.subplot(1, 2, 2)
+        plt.title('Prediction')
+        plt.bar(CLASS_NAMES, score)
+        plt.show()
+        input()
