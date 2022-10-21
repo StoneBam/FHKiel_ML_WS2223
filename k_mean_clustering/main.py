7af0f4d5707ee29c0480581908c175cfb44ffb33
StoneBam <fynn_wulf@gmx.de>
2022-10-21 22:08:29 +0200

    Add method to creating random point clouds

--- k_mean_clustering/main.py
+++ k_mean_clustering/main.py
@@ -1,2 +1,21 @@
+#!/usr/bin/env python3.10
+import random
+
+from point import Point
+
+
+class RandomPointCloud(random.Random):
+
+    def __init__(self, seed: str | int | None = None) -> None:
+        super().__init__(seed)
+
+    def create(self, n_pts: int, bounds: tuple[float, float], biases: tuple[float, float, float], dims: int = 2) -> list[Point]:
+        return [Point(
+            x=self.uniform(*bounds) + biases[0] if dims >= 1 else 0.0,
+            y=self.uniform(*bounds) + biases[1] if dims >= 2 else 0.0,
+            z=self.uniform(*bounds) + biases[2] if dims >= 3 else 0.0
+            ) for _ in range(n_pts)]
+
+
 if __name__ == '__main__':
     pass
