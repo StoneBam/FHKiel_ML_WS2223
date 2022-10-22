120aaa6dd897c65928f271b89c745c6db0721840
Fynn <fynn_wulf@gmx.de>
2022-10-22 12:09:05 +0200

    Add ffmpeg for saving animations

--- k_mean_clustering/main.py
+++ k_mean_clustering/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.10
 import random
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
@@ -26,6 +27,7 @@ global z_m
 
 
 if __name__ == '__main__':
+    mpl.rcParams['animation.ffmpeg_path'] = r"C:\ffmpeg-5.1.2-essentials_build\bin\ffmpeg.exe"
     rpc = RandomPointCloud('test')
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -49,9 +51,10 @@ if __name__ == '__main__':
         global quiv
         global scat
         quiv.remove()
-        quiv = ax.quiver(x_m, y_m, z_m, 1, 1, 1, length=0, color='firebrick')
+        quiv = ax.quiver(x_m, y_m, z_m, 1, 1, 1, length=1, color='firebrick')
         scat.remove()
         scat = ax.scatter(x_m, y_m, z_m, marker='o', c='firebrick')
 
     ani = animation.FuncAnimation(fig, func=fla, frames=np.linspace(0, 10, 100), interval=50)
+    ani.save("mov.mp4")
     plt.show()
