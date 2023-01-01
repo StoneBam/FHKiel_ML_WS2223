eddf8ec37b9498858431eedd18a6d80305c68bc0
StoneBam <fynn_wulf@gmx.de>
2023-01-01 16:20:09 +0100

    Add argparse to main.py

--- reinforcement_learning/main.py
+++ reinforcement_learning/main.py
@@ -1,12 +1,54 @@
+import argparse
+
 from model import Roboid
 from presenter import Presenter
 from view import Environment
 
 
+def sanity_check_args(args: argparse.Namespace) -> None:
+    """Sanity check the arguments.
+
+    Args:
+        args (argparse.Namespace): arguments
+
+    Raises:
+        ValueError: if the arguments are invalid
+    """
+    # Check the map size
+    if args.mapsize[0] <= 0 or args.mapsize[1] <= 0:
+        raise ValueError("Map size must be greater than 0")
+
+    # Check the start position
+    if args.start[0] < 0 or args.start[1] < 0:
+        raise ValueError("Start position must be greater than 0")
+    if args.start[0] >= args.mapsize[0] or args.start[1] >= args.mapsize[1]:
+        raise ValueError("Start position must be inside the map")
+
+    # Check the target position
+    if args.target[0] < 0 or args.target[1] < 0:
+        raise ValueError("Target position must be greater than 0")
+    if args.target[0] >= args.mapsize[0] or args.target[1] >= args.mapsize[1]:
+        raise ValueError("Target position must be inside the map")
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mapsize', type=int, nargs=2, default=(10, 10))
+    parser.add_argument('-s', '--start', type=int, nargs=2, default=(8, 8))
+    parser.add_argument('-t', '--target', type=int, nargs=2, default=(2, 2))
+    args = parser.parse_args()
+
+    # Sanity check the arguments
+    sanity_check_args(args)
+
+    # Arguments
+    mapshape = args.mapsize
+    pos_start = args.start
+    pos_target = args.target
+
     # Create model, view and presenter
-    model = Roboid()
-    view = Environment()
+    model = Roboid(mapshape, pos_start, pos_target)
+    view = Environment(mapshape)
     presenter = Presenter(model, view)
 
     # Run the application
