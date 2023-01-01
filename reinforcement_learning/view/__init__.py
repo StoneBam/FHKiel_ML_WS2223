75ae9bebebd895fd3a42ac9d9b16f93a9a98b587
StoneBam <fynn_wulf@gmx.de>
2023-01-01 12:44:47 +0100

    Add docstrings

--- reinforcement_learning/view/__init__.py
+++ reinforcement_learning/view/__init__.py
@@ -16,31 +16,63 @@ class Environment:
             'contourmap': self.contourmap
         }
 
+    # Map creation methods
+
     def create_empty_map(self, borders: bool = False) -> np.ndarray:
+        """Create an empty map with all values set to 0.
+
+        Args:
+            borders (bool, optional): Place borders. Defaults to False.
+
+        Returns:
+            np.ndarray: empty map
+        """
         _map = np.zeros(self.mapsize, dtype=np.float64)
         if borders:
             _map = self.place_borders(_map)
         return _map
 
+    def create_random_map(self, borders: bool = True) -> np.ndarray:
+        """Create a random map with values between 0 and 1.
+
+        Args:
+            borders (bool, optional): Place borders. Defaults to True.
+
+        Returns:
+            np.ndarray: random map
+        """
+        _map = np.random.rand(*self.mapsize)
+        if borders:
+            _map = self.place_borders(_map)
+        return _map
+
     def place_borders(self, _map: np.ndarray) -> np.ndarray:
+        """Place borders around the map. A border is a negative value.
+
+        Args:
+            _map (np.ndarray): map to be modified
+
+        Returns:
+            np.ndarray: map with borders
+        """
         _map[0, :] = -1
         _map[-1, :] = -1
         _map[:, 0] = -1
         _map[:, -1] = -1
         return _map
 
-    def create_random_map(self, borders: bool = True) -> np.ndarray:
-        _map = np.random.rand(*self.mapsize)
-        if borders:
-            _map = self.place_borders(_map)
-        return _map
-
-    def heatmap(self, _map: np.ndarray, ax: plt.Axes, cbar: bool = True) -> None:
-        ax.xaxis.tick_top()
-        annot = True if max(_map.shape) < 20 else False
-        sns.heatmap(_map, annot=annot, fmt=".2f", cmap="hot", ax=ax, cbar=cbar, square=True, vmin=0, vmax=1)
+    # Map visualization methods
 
     def contourmap(self, _map: np.ndarray, ax: plt.Axes) -> None:
+        """Visualize a map as a contour map.
+
+        Args:
+            _map (np.ndarray): map to be visualized
+            ax (plt.Axes): Axes object to be used for visualization
+
+        Returns:
+            None
+        """
         blurred_map = ndimage.gaussian_filter(_map, sigma=0.5)
         map_axis = [np.array(range(size)) for size in self.mapsize]
         loc = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
@@ -50,13 +82,44 @@ class Environment:
         ax.grid(True, which='both', color='g', linewidth=0.5)
         ax.invert_yaxis()
 
+    def heatmap(self, _map: np.ndarray, ax: plt.Axes, cbar: bool = True) -> None:
+        """Visualize a map as a heatmap.
+
+        Args:
+            _map (np.ndarray): map to be visualized
+            ax (plt.Axes): Axes object to be used for visualization
+            cbar (bool, optional): Show colorbar. Defaults to True.
+        """
+        ax.xaxis.tick_top()
+        annot = True if max(_map.shape) < 20 else False
+        sns.heatmap(_map, annot=annot, fmt=".2f", cmap="hot", ax=ax, cbar=cbar, square=True, vmin=0, vmax=1)
+
+    # Map display methods
+
     def show_map(self, _map, _map_key: str) -> None:
+        """Show a single map with a specific visualization method.
+
+        Args:
+            _map (np.ndarray): map to be displayed
+            _map_key (str): key of the visualization method to be used
+
+        Returns:
+            None
+        """
         _, ax = plt.subplots(1, 1, figsize=(5, 5), subplot_kw={'aspect': 'equal'})
         self.info_layers[_map_key](_map, ax)
         plt.tight_layout()
         plt.show()
 
     def show_all_maps(self, _map: np.ndarray) -> None:
+        """Show all maps with all visualization methods.
+
+        Args:
+            _map (np.ndarray): map to be displayed
+
+        Returns:
+            None
+        """
         _, axes = plt.subplots(1, 2, figsize=(10, 5), subplot_kw={'aspect': 'equal'})
         self.heatmap(_map, axes[0], False)
         self.contourmap(_map, axes[1])
